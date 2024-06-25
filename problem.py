4c0144a53212f569cbfafb53d42ef93ef4383963
moramahesh <maheswara215mora@gmail.com>
2024-06-25 11:31:12 +0530

    code updated

--- problem.py
+++ problem.py
@@ -1,1 +1,8 @@
-print(f"hello")
+#palindrome
+for i in range(1,201):
+    i =str(i)
+    p = i[::-1]
+    if p==i:
+        print(p)
+#method 2
+print([i for i in range(1,201) if str(i)[::-1]==str(i)])
